970b95cd5d1000d9df0748fcf7e4160527fcc196
ph-pc <ph-pc@Note-Ph.home>
2017-06-27 18:39:27 -0300

    criada classe para controlar a pagina orc_simples

--- TAI/src/website/OrcamentoSimples.java
+++ TAI/src/website/OrcamentoSimples.java
@@ -1,9 +1,6 @@
 package website;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Home
+ * Servlet implementation class OrcamentoSimples
  */
-@WebServlet(name="HomeController", urlPatterns={"/"})
-public class HomeController extends HttpServlet {
+@WebServlet(name="Orçamento Simples", urlPatterns={"/orc_simples"})
+public class OrcamentoSimples extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HomeController() {
+    public OrcamentoSimples() {
         super();
     }
 
@@ -29,20 +26,13 @@ public class HomeController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-//		response.sendRedirect("TAI/Home.jsp");
-		System.out.println(request.getParameter("valor"));
-//
-//		ServletContext sc = this.getServletContext();
-//		RequestDispatcher rd = sc.getRequestDispatcher("/home.jsp");
-//		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		doGet(request, response);
-		System.out.println(request.getParameter("valor"));
+		doGet(request, response);
 	}
 
 }
