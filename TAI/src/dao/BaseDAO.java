3cbb0dbe2db32e5abdd025b04a7024086e82c5e1
ph-pc <ph-pc@Note-Ph.home>
2017-07-03 13:53:31 -0300

    Criada classe abstrata BaseDAO. Removida classe Conexao

--- TAI/src/dao/BaseDAO.java
+++ TAI/src/dao/BaseDAO.java
@@ -4,13 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-final class Conexao {
+public abstract class BaseDAO {
 	
-	private static String user="admin",
-			passwd="admin123",
+	//passar admin e senha para properties? cripto?
+	private static String user="admin", passwd="admin123",
 			endpoint="tai-db.cyki8d0w5wwv.sa-east-1.rds.amazonaws.com/database";
+	
+	public BaseDAO() {
+		try{
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
 	
 	protected static final Connection createConnection() throws SQLException{
 		return DriverManager.getConnection("jdbc:mariadb://"+endpoint+"?user="+user+"&password="+passwd);
 	}
+	
+	//TODO implementar cache aqui
 }
